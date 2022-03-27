ab964e123bf8168c8a6c7de97838a8e55d041e15
superkor <justinchow03@hotmail.com>
2022-03-26 22:07:54 -0400

    added title bar in chapters

---
+++ chapters/chapters.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    const chapterData = new XMLHttpRequest();
+    chapterData.overrideMimeType("application/json");
+    chapterData.open("GET", "/chapterDetails.json", true);
+
+    chapterData.onload = function(){
+        var divParent, chapterLinks;
+        var data = JSON.parse(this.response);
+        divParent = document.getElementsByClassName("dropdown-book1");
+        for (var x = 0; x < Object.keys(data.book_one.chapters).length; x++){
+            chapterLinks = document.createElement("a");
+            chapterLinks.setAttribute("href", "#");
+            chapterLinks.appendChild(document.createTextNode(data.book_one.chapters[x].title));
+            divParent[0].appendChild(chapterLinks);
+        }
+
+        divParent = document.getElementsByClassName("dropdown-book2");
+        for (var x = 0; x < Object.keys(data.book_two.chapters).length; x++){
+            chapterLinks = document.createElement("a");
+            chapterLinks.setAttribute("href", "#");
+            chapterLinks.appendChild(document.createTextNode(data.book_two.chapters[x].title));
+            divParent[0].appendChild(chapterLinks);
+        }
+    }
+
+    chapterData.send();
+
+}
